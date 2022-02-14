062a6a5c74692d601caade52ad29d47701b5941a
Alexey Romashin <russiandoomer22@gmail.com>
2022-02-14 22:04:24 +0300

    10. default опасность плагин auto import

---
+++ react-kabzda-kak-prosto/projects/01-first-project/react-kabzda-1/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Technologies from './Technologies';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
