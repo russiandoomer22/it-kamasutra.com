ada6bb8c4baab4ad9454fb55efb74dc809a6f56b
Alexey Romashin <russiandoomer22@gmail.com>
2022-02-15 00:03:15 +0300

    12. Разбивка на компоненты

---
+++ react-kabzda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://img4.goodfon.ru/original/1280x1024/c/d6/gory-kanada-lodka-chelovek-ostrov-les-ozero.jpg' />
+    </header>;
+};
+
+export default Header;
---
+++ react-kabzda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://img5.goodfon.ru/original/1280x1024/0/fd/kananaskis-range-canadian-rocky-mountains-alberta-canada-kan.jpg' width='750' />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+        </div>
+        <div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>;
+};
+
+export default Profile;
