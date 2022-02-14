060d7f466d37128c343d08fa016b531c11e65f32
Alexey Romashin <russiandoomer22@gmail.com>
2022-02-14 23:29:03 +0300

    11. соц. сеть, шаг 1, вёрстка grid

--- react-kabzda-kak-prosto/projects/01-first-project/react-kabzda-1/src/App.js
+++ react-kabzda-kak-prosto/projects/01-first-project/react-kabzda-1/src/App.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
-import Footer from './Footer';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://img4.goodfon.ru/original/1280x1024/c/d6/gory-kanada-lodka-chelovek-ostrov-les-ozero.jpg' />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://img5.goodfon.ru/original/1280x1024/0/fd/kananaskis-range-canadian-rocky-mountains-alberta-canada-kan.jpg' />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
