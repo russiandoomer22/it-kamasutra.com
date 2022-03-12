c72675e3d638674ba69f298080dcf5c84a876739
Alexey <russiandoomer22@gmail.com>
2022-03-12 23:12:57 +0300

    20. NavLink

--- react-kabzda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Navbar/Navbar.jsx
+++ react-kabzda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
-    return <nav className={s.nav}>
-        <div className={s.item}>
-            <a href="/profile">Profile</a>
-        </div>
-        <div className={`${s.item} ${s.active}`}>
-            <a href="/dialogs">Messages</a>
-        </div>
-        <div className={s.item}>
-            <a href="/news">News</a>
-        </div>
-        <div className={s.item}>
-            <a href="/music">Music</a>
-        </div>
-        <div className={s.item}>
-            <a href="/settings">Settings</a>
-        </div>
-    </nav>
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to="/dialogs">Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/news">News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/music">Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/settings">Settings</NavLink>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
